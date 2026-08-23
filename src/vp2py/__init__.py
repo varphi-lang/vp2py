501521e122b7b4de171cfde3713229411f577561
Hassan El-Sheikha <hmelsheikha@gmail.com>
2026-08-23 18:52:50 -0400

    Add version to __init__ and make default input stdin

--- src/vp2py/__init__.py
+++ src/vp2py/__init__.py
@@ -1,3 +1,5 @@
 from .compiler import VarphiToPythonCompiler
 
+__version__ = "3.0.1"
+
 __all__ = ["VarphiToPythonCompiler"]
--- src/vp2py/cli.py
+++ src/vp2py/cli.py
@@ -1,16 +1,18 @@
+import sys
 from pathlib import Path
+from typing import Optional
 import typer
 
 
 def vp2py(
-    input_file: Path = typer.Argument(
-        ...,
+    input_file: Optional[Path] = typer.Argument(
+        None,
         exists=True,
         file_okay=True,
         dir_okay=False,
         readable=True,
         resolve_path=True,
-        help="Path to input Varphi source file",
+        help="Path to input Varphi source file. If omitted, reads from standard input.",
     ),
     debug: bool = typer.Option(
         False,
@@ -25,8 +27,14 @@ def vp2py(
     if debug:
         compiler.toggle_debug()
 
-    # Read file, compile, and print result to stdout
-    result = compiler.compile(input_file.read_text(encoding="utf-8"))
+    if input_file:
+        source_code = input_file.read_text(encoding="utf-8")
+    else:
+        # Fallback to stdin if no file is provided
+        source_code = sys.stdin.read()
+
+    # Compile, and print result to stdout
+    result = compiler.compile(source_code)
     typer.echo(result)
 
 
