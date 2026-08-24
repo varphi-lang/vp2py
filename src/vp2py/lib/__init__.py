46006601d9f08a06731582d6967a7b5a7c67c0ca
Hassan El-Sheikha <hmelsheikha@gmail.com>
2026-08-23 20:10:41 -0400

    chore: Bump version to 3.0.3

--- src/vp2py/lib/__init__.py
+++ src/vp2py/lib/__init__.py
@@ -2,7 +2,7 @@ from .model import State, Tape, Head, TuringMachine
 from .functions import main
 from .exceptions import VarphiRuntimeError
 
-__version__ = "3.0.2"
+__version__ = "3.0.3"
 
 __all__ = [
     "State",
